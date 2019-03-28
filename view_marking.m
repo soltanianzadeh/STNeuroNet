c2624829241c497a01566322dc5e43511b835dde
soltanianzadeh <31009809+soltanianzadeh@users.noreply.github.com>
2019-03-28 16:07:59 -0400

    Update view_marking.m

--- view_marking.m
+++ view_marking.m
@@ -27,10 +27,10 @@ opt.type = 'Layer275';          % Type of data. For the 'Allen' data, this is ei
                                 % or 'train'. 
 
 opt.marking = 'Allen';        % Type of marking to show. For the 'Allen' data, this is either 
-                                % 'Allen' or 'FinalGT', denoting Allen's initial marking or 
-                                % the final curated masks. For 'Neurofinder' this is
-                                % 'neurofinder' or 'Grader1'. Not that if 'test' is selected   
-                                % for the 'Neurofinder' data, there is no 'neurofinder' mask.
+                                % 'Allen','FinalGT', 'Grader3', or 'Grader4', denoting Allen's initial marking, 
+                                % final curated masks, grader 3's markings or grader 4'2 markings.
+                                % For 'Neurofinder' this is'neurofinder' or 'Grader1'.    
+                                % Not that if 'test' is selected for the 'Neurofinder' data, there is no 'neurofinder' mask.
 
 RunViewMarking(opt);
 
